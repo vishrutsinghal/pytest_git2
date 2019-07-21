375b07aa340636b4f85b69794d45d9b336af3f68
Vishrut Singhal <Vishrut.Singhal@email.com>
2019-07-21 14:10:07 +0530

    "calc file"

---
+++ calc2.py
@@ -0,0 +1,14 @@
+def add(value1, value2):
+    return value1 + value2
+
+
+def sub(value1, value2):
+    return value1 - value2
+
+
+def mul(value1, value2):
+    return value1 * value2
+
+
+def div(value1, value2):
+    return value1 / value2
